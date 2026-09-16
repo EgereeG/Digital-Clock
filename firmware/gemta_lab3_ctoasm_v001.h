0426a4f9b2329fab75ea38db278ba468fe86fc7f
Egeree Gemta <egereegemta@gmail.com>
2026-09-15 21:07:43 -0500

    Add files via upload

---
+++ firmware/gemta_lab3_ctoasm_v001.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   gemta_lab3_ctoasm_v001.h
+ * Author: egere
+ *
+ * Created on July 1, 2025, 7:05 PM
+ */
+
+#ifndef GEMTA_LAB3_CTOASM_V001_H
+#define	GEMTA_LAB3_CTOASM_V001_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void wait_1ms(void);
+void wait_100us(void); 
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* GEMTA_LAB3_CTOASM_V001_H */
+
